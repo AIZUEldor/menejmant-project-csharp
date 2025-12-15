e20721bdf7e19d76b1bfe547e3383f8b43e04ef6
AIZUEldor <eldor9406@icloud.com>
2025-12-15 21:51:55 +0500

    Array qo'shildi

---
+++ Management.Aplication/Servises/StudentService.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Management.Infrastucture1;
+using Managment.Domain.Models;
+
+namespace Management.Aplication.Servises
+{
+    public class StudentService
+    {
+        public DbContext DbContext { get; set; }
+
+        public StudentService()
+        {
+            DbContext = new DbContext();
+        }
+        public void AddStudent(string firstName, string lastName)
+        {
+            var newStudent = new Student
+            {
+                id = new Random().Next(1, 1000).ToString(),
+                firstName = firstName,
+                lastName = lastName
+            };
+            this.DbContext.Students[0] = newStudent;
+        }
+    }
+}
