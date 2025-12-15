1cafe422f357f0d23aa75024ac763caf66286622
AIZUEldor <eldor9406@icloud.com>
2025-12-15 21:05:37 +0500

    Добавьте файлы проекта.

---
+++ Managment.Domain/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Managment.Domain.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
